9c1d0ad25a19c8e8ee5335554c29f6569a4c9f17
Ashley <pooh520520@gmail.com>
2022-12-24 14:00:03 +0800

    f1

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    print(f"request的attribute method:{request.method}")
+    print(f"request的property mimetype:{request.mimetype_params}")
     return render_template('index.html')
     
